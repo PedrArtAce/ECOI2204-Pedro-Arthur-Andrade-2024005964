f894de4cb7c802db7bdeebd22d05c346b173e98a
PedrArtAce <160905524+PedrArtAce@users.noreply.github.com>
2024-10-18 20:32:18 -0300

    Update exerc2.cpp
    adicionado getters e setters em "carro"

--- POO/exerc2.cpp
+++ POO/exerc2.cpp
@@ -30,6 +30,62 @@ public:
         strcpy(motor, "");
     }
 
+    //setters
+    void setParado(int _parado) 
+    {
+        parado = _parado;
+    }
+
+    void setLigado(int _ligado) 
+    {
+        ligado = _ligado;
+    }
+
+    void setFabricante(const char *_fabricante)
+    {
+        delete[] fabricante; 
+        fabricante = new char[strlen(_fabricante) + 1];
+        strcpy(fabricante, _fabricante);
+    }
+
+    void setModelo(const char *_modelo)
+    {
+        delete[] modelo;
+        modelo = new char[strlen(_modelo) + 1];
+        strcpy(modelo, _modelo);
+    }
+
+    void setMotor(const char *_motor)
+    {
+        delete[] motor; 
+        motor = new char[strlen(_motor) + 1];
+        strcpy(motor, _motor);
+    }
+
+    //getters
+    int getParado()
+    {
+        return parado; 
+    }
+
+    int getLigado() const 
+    {
+        return ligado; 
+    }
+
+    const char *getFabricante() const
+    { 
+        return fabricante; 
+    }
+    const char *getModelo() const 
+    {
+        return modelo; 
+    }
+    const char *getMotor() const 
+    { 
+        return motor;
+    }
+
     void ligar()
     {
         ligado = 1;
